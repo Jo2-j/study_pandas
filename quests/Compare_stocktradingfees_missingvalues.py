b4316f931308a8ca076a3d7b9af8c65e779e882c
Jo2-j <w01205250623@nate.com>
2024-12-18 06:50:07 +0000

    feat : 1218(2)

--- quests/Compare_stocktradingfees_missingvalues.py
+++ quests/Compare_stocktradingfees_missingvalues.py
@@ -1,74 +1,77 @@
-# - target : Mongo(compare_stocktradingfees_merges)
-# - from Mongo with query : {'구분' : '변경후'}
-
 import pandas as pd
 import numpy as np
 from pymongo import MongoClient
+
 
 client = MongoClient('mongodb://192.168.0.46:27017/')
 db_name = client['DB_SGMN']
 collection_name = db_name['COL_FEE_RATES']
 
-# 1. 변경후만 가져오기
-
-rawData = collection_name.find({
-    "Unnamed: 4": "변경후"
-})
-
+# 1. 변경후 데이터 가져오기
+rawData = collection_name.find({"Unnamed: 4": "변경후"})
 postChgData = list(rawData)
 
-# 2. DataFrame으로 변환
 
-df = pd.DataFrame(postChgData)
+# 2. DataFrame으로 변환 및 필요한 컬럼만 선택
+df = pd.DataFrame(postChgData)[['Unnamed: 0', 'Unnamed: 2', 'Unnamed: 3', '스마트폰.1', '스마트폰']]
 
-# 3. 필요없는 컬럼 삭제(필요한 컬럼만 선택)
+df = df.rename(columns={
+    'Unnamed: 0': '증권사명',
+    'Unnamed: 2': '기준일',
+    'Unnamed: 3': '거래대금',
+    '스마트폰.1': '은행 개설 계좌',
+    '스마트폰': '증권사 개설 계좌'
+})
 
-
-df = pd.DataFrame(postChgData)[['Unnamed: 0',
-                                'Unnamed: 2',
-                                'Unnamed: 3', 
-                                'HTS', 
-                                '스마트폰']]
-
-print("-------")
-
-                            
-
-
-# 4.1 결측치 임의로 0으로 채우기
-# 0으로 채우고 그 행 드롭
-# 유실되는 데이터가 많을 것 같음
-
-# 특정 컬럼만 처리
-columns_to_check = ['HTS', '스마트폰']  # 처리할 컬럼명 지정
+# 3. 데이터 전처리
+# NaN 값 처리
+columns_to_check = ['은행 개설 계좌', '증권사 개설 계좌']  # 처리할 컬럼명 지정
 df_filled = df.copy()
 df_filled[columns_to_check] = df_filled[columns_to_check].fillna(0)
 rows_with_zeros = df_filled[columns_to_check].isin([0]).any(axis=1)
 df_no_zeros = df_filled[~rows_with_zeros]
-# 5. 필요한 컬럼 생성 
 
-df_no_zeros['수수료 합'] = df['HTS'] + df['스마트폰']
+# 새로운 컬럼 추가
+df_no_zeros['수수료합'] = df_no_zeros['증권사 개설 계좌'] + df_no_zeros['은행 개설 계좌']
+# 거래대금 컬럼을 숫자로 변환
 
-finalSeries = df_no_zeros['수수료 합']
+
+# 한 번에 처리하는 방법
+df_no_zeros['거래대금'] = df_no_zeros['거래대금'].str.replace('원', '').replace(
+    {'억': '*100000000', '만': '*10000'}, regex=True).map(eval)
+
+
+# 수수료율 계산 (수수료합 / 거래대금)
+df_no_zeros['수수료율'] = df_no_zeros['수수료합'] / df_no_zeros['거래대금'] * 2
+
+finalSeries = df_no_zeros['수수료율']
 
 meanResult = finalSeries.mean()
 
-print("----------")
+# 4. 회사별
 
-# 4.2 결측치 상대값으로 채우기
+# 1. 회사별 평균 계산
+company_means = df_no_zeros.groupby('증권사명')['수수료율'].mean()
 
-# 만약 HTS에서 값이 있지만, 스마트폰에 없으면 HTS의 것을 채우자
-# 만약 HTS에서 값이 없지만, 스마트폰에 값이 있으면 스마트폰의 것을 채우자
+# 2. 원래 DataFrame의 수수료율을 회사별 평균으로 대체
+df_no_zeros['수수료'] = df_no_zeros['증권사명'].map(company_means)
 
-# df['HTS'] = df['HTS'].fillna(0)
-# df['스마트폰'] = df['스마트폰'].fillna(0)
+# 3. 증권사별로
+
+# 증권사명으로 그룹화하여 나열
+grouped_df = df_no_zeros.groupby('증권사명').agg({
+    '수수료율': 'mean'
+}).round(4)
+
+grouped_df = grouped_df.reset_index()
+
+# 전체 평균보다 작은 증권사들 필터링
+low_fee_companies = grouped_df[grouped_df['수수료율'] < meanResult]
+
+# 수수료율 오름차순으로 정렬
+low_fee_companies = low_fee_companies.sort_values('수수료율', ascending=True)
 
 
-# 5.
 
-# 4.1 방법 사용
-underMean = df_no_zeros[df_no_zeros['수수료 합'] < meanResult]
 
-print("----------")
-
-# - 스마트폰 수수료 평균 이상인 증권사들 파일(CSV) 저장
+print("------------")
---
+++ quests/TrialandErrors_Compare_stocktradingfees_missingvalues.py
@@ -0,0 +1,69 @@
+# - target : Mongo(compare_stocktradingfees_merges)
+# - from Mongo with query : {'구분' : '변경후'}
+
+import pandas as pd
+import numpy as np
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://192.168.0.46:27017/')
+db_name = client['DB_SGMN']
+collection_name = db_name['COL_FEE_RATES']
+
+# 1. 변경후만 가져오기
+
+rawData = collection_name.find({
+    "Unnamed: 4": "변경후"
+})
+
+postChgData = list(rawData)
+
+# 2. DataFrame으로 변환
+
+df = pd.DataFrame(postChgData)
+
+# 3. 필요없는 컬럼 삭제(필요한 컬럼만 선택)
+
+
+df = pd.DataFrame(postChgData)[['Unnamed: 0',
+                                'Unnamed: 2',
+                                'Unnamed: 3', 
+                                '스마트폰.1', 
+                                '스마트폰']]
+
+print("-------")
+
+                            
+
+
+# 4.1 결측치 임의로 0으로 채우기
+# 0으로 채우고 그 행 드롭
+# 유실되는 데이터가 많을 것 같음
+
+# 특정 컬럼만 처리
+columns_to_check = ['스마트폰.1', '스마트폰']  # 처리할 컬럼명 지정
+df_filled = df.copy()
+df_filled[columns_to_check] = df_filled[columns_to_check].fillna(0)
+rows_with_zeros = df_filled[columns_to_check].isin([0]).any(axis=1)
+df_no_zeros = df_filled[~rows_with_zeros]
+# 5. 필요한 컬럼 생성 
+
+df_no_zeros['수수료 합'] = df['스마트폰.1'] + df['스마트폰']
+
+finalSeries = df_no_zeros['수수료 합']
+
+meanResult = finalSeries.mean()
+
+print("----------")
+
+# 4.2 결측치 상대값으로 채우기
+
+
+
+# 5.
+
+# 4.1 방법 사용
+underMean = df_no_zeros[df_no_zeros['수수료 합'] < meanResult]
+underMeanWithoutDuplicates = underMean.drop_duplicates(subset=['Unnamed: 0'])
+print("----------")
+
+# - 스마트폰 수수료 평균 이상인 증권사들 파일(CSV) 저장
